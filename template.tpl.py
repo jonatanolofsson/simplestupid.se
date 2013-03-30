35f250b48ff07a93755f601aacf1c4d88fd3d270
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-03-30 23:28:01 +0100

    Markdown styling

--- template.tpl.py
+++ template.tpl.py
@@ -4,6 +4,7 @@ def header(environ, fmt):
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <link rel="stylesheet" type="text/css" href="/style.css">
+<link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link>
 <title>{title}</title>
 </head>
 <body>
