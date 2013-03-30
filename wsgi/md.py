d7ba573ada06fc20ccf89a18f8a68c6e70a14f23
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-03-30 11:52:11 +0100

    Test menu generation

--- wsgi/md.py
+++ wsgi/md.py
@@ -4,14 +4,27 @@ siteroot = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 htdocs = os.path.join(siteroot, 'htdocs')
 tplname = 'template.tpl.py'
 
-def getMenu(mdfile):
-    return 'menu'
+def getMenu(startDirectory, endDirectory, level = 0):
+    output = ""
+    if endDirectory != startDirectory:
+        (output, level) = getMenu(startDirectory, os.path.dirname(endDirectory), level)
+
+    output += '<ol class="menulevel{level}">'.format(level=level) + "".join(['<li><a href="{link}">{name}</a></li>'.format(
+        link = os.path.relpath(os.path.abspath(filename), htdocs),
+        name = os.path.basename(filename).split('-', 2)[1]
+    ) for filename in os.listdir(endDirectory)] + "</ol>")
+
+    if level == 0:
+        return output
+    else:
+        return (output, level+1)
+
 
 def wsgi_app(environ, start_response):
     output = ""
     mdfile = os.path.join(htdocs, environ['REDIRECT_URL'].lstrip('/'))
     tpldir = os.path.dirname(mdfile)
-    while not os.path.exists(os.path.join(tpldir, tplname)) and not (os.path.realpath(tpldir) == os.path.realpath(siteroot)):
+    while not os.path.exists(os.path.join(tpldir, tplname)) and not tpldir == siteroot:
         tpldir = os.path.dirname(tpldir)
 
     tpl = os.path.join(tpldir, tplname)
@@ -20,7 +33,7 @@ def wsgi_app(environ, start_response):
         template = imp.load_source('template', tpl)
 
     if "header" in dir(template):
-        output += template.header(environ, {'title': mdfile, 'menu': getMenu(mdfile)})
+        output += template.header(environ, {'title': mdfile, 'menu': getMenu(htdocs, os.path.dirname(mdfile))})
 
     with open(mdfile) as f:
         output +=  markdown2.markdown(f.read())
