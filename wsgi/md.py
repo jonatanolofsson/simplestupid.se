ef2b20611a69d53bea4d8183b0a3e4ea74baf002
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-03-31 08:11:07 +0200

    Title

--- wsgi/md.py
+++ wsgi/md.py
@@ -70,7 +70,7 @@ def application(environ, start_response):
 
     if "header" in dir(template):
         output += template.header(environ, {
-            'title': mdfile,
+            'title': os.path.relpath(mdfile, htdocs)[0:-3].replace('/', ' / '),
             'menu': getMenu(mdfile, htdocs, os.path.dirname(mdfile))
         })
 
